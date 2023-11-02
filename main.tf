ff4a93b2ad4bacd7847cc7a96141419ce581e7d1
vboxuser <vboxuser@VADIM.myguest.virtualbox.org>
2023-11-02 16:00:35 +0400

    deploying an application using docker compos in provisioner

--- main.tf
+++ main.tf
@@ -74,7 +74,19 @@ resource "yandex_compute_instance" "vm-1" {
   }
   provisioner "remote-exec" {
     inline = [
-      "touch /tmp/test"
+      "ssudo apt-get install ca-certificates curl gnupg",
+      "sudo install -m 0755 -d /etc/apt/keyrings",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+      "sudo chmod a+r /etc/apt/keyrings/docker.gpg",
+      "echo \"deb [arch=\\\"$(dpkg --print-architecture)\\\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \\\"$(. /etc/os-release && echo \\\"$VERSION_CODENAME\\\")\\\" stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y",
+      "sudo usermod -aG docker $USER",
+      "git clone https://github.com/ShunkanTen/spring-petclinic.git",
+      "cd ./spring-petclinic/",
+      "sudo docker build . -t petclinic",
+      "sudo docker network create net",
+      "sudo docker compose up -d"
     ]
   }
 }
